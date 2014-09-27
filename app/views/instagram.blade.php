b12beb0e45902f1581b172e67a3cbfeb2703eef1
Johan Thorstedt <webdev@icode4u.se>
2014-09-27 17:32:16 +0200

    Felhantering vid privata konton på instagram

--- app/views/instagram.blade.php
+++ app/views/instagram.blade.php
@@ -1,11 +1,18 @@
-@foreach($feed->data as $item)
-    <div>
-        <img src="{{ $item->images->low_resolution->url }}" alt=""/>
-        @if(isset($item->caption->text))
-        <p>
-           {{ $item->caption->text }}
-        </p>
-        @endif
-        {{ var_dump($item) }}
-    </div>
-@endforeach
+@if(isset($feed->data))
+    @foreach($feed->data as $item)
+        <div>
+            <img src="{{ $item->images->low_resolution->url }}" alt=""/>
+            @if(isset($item->caption->text))
+            <p>
+               {{ $item->caption->text }}
+            </p>
+            @endif
+            {{ var_dump($item) }}
+        </div>
+    @endforeach
+@endif
+@if($feed->meta->code == 400)
+    <p>
+        Denna användaren är privat, då kan vi inte se denna...
+    </p>
+@endif
