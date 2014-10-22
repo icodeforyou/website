dc11e70fc778ff3e317cf2dc680119817e0bc034
Johan Thorstedt <webdev@icode4u.se>
2014-10-22 17:29:00 +0200

    Tagirt bort texten om portfolio

--- app/lang/sv/texts.php
+++ app/lang/sv/texts.php
@@ -37,7 +37,7 @@ return [
     </p>",
     "lead" => "iCode4u.se är ett enmansföretag som utvecklar allt inom webb. iCode4u innehar F-skattesedel.",
     "make_contact" => "Vill du komma i kontakt med iCode4u, skicka ett meddelande till iCode4u med formuläret nedan.",
-    "webb_development" => "iCode4u utvecklar i främst PHP med kopplingar till MySQL databaser. Vi ökar användarvänligheten med hjälp av javascriptspråket jQuery och dess fantastiska möjligheter. Vill ni se arbetsprover kan ni se några under portfolio.",
+    "webb_development" => "iCode4u utvecklar i främst PHP med kopplingar till MySQL databaser. Vi ökar användarvänligheten med hjälp av javascriptspråket jQuery och dess fantastiska möjligheter.",
     "optimizations" => "Behöver du hjälp med :link och förbättringar av befintliga webblösningar så hjälper iCode4u gärna till. Vi kan hjälpa dig att synas på nätet.",
     "cloud_hosting" => "Vill man köra i molnet istället kan iCode4u hjälpa till mig att flytta över er hemsida till :amazon eller :digitalocean. Att använda molnet för att drifta sin hemsida har visat sig vara väldigt kostnadseffektivt då man enbart betalar för prestandan/bandbredden man utnyttjar. Amazon och Digital Ocean har samma möjligheter som Glesys att automatiskt skala upp prestandan på en hemsida allt eftersom man får mer trafik till sin hemsida.",
     "thanks_for_email" => "Tack! Ditt meddelande har blivit skickat"
