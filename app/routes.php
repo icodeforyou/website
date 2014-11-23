2c9b473e6b00ec7fba8f86164a115fc093b86d07
Johan Thorstedt <webdev@icode4u.se>
2014-11-23 22:59:52 +0100

    fiber till slöinge intressenter json

--- app/routes.php
+++ app/routes.php
@@ -11,16 +11,15 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('index');
+Route::get('/', function () {
+    return View::make('index');
 });
 
-Route::get("tjanster", function() {
+Route::get("tjanster", function () {
     return View::make("tjanster");
 });
 
-Route::get("kontakt", function() {
+Route::get("kontakt", function () {
     return View::make("kontakt");
 });
 
@@ -28,14 +27,14 @@ Route::post("kontakt", ["uses" => "BaseController@Contact"]);
 
 Route::get("digital/{action}/{method}/{value?}", ["uses" => "DigitalOceanController@index"]);
 
-Route::group(["prefix" => "__spa/v1"], function() {
+Route::group(["prefix" => "__spa/v1"], function () {
     Route::get("{page}", "SpaController@RenderPage");
 });
 
 Route::get("instagram/{userId?}", "InstagramController@index");
 Route::get("instagramcode", "InstagramController@store");
 
-Route::get("locale/{locale}", function($locale) {
+Route::get("locale/{locale}", function ($locale) {
 
     // Store the locale in session
     Session::put("locale", $locale);
@@ -45,3 +44,54 @@ Route::get("locale/{locale}", function($locale) {
 });
 
 Route::post("fibertillsloinge", "BaseController@fiberProxy");
+Route::get("fibertillsloinge/intressenter", function () {
+
+    $intressenter = [
+        [
+            "name"    => "Fam. Thorstedt",
+            "address" => "Oktorp 107, 311 67 Slöinge"
+        ],
+        [
+            "name"    => "Fam. Albrektsson",
+            "address" => "Trastgatan 3, 311 68 Slöinge"
+        ],
+        [
+            "name"    => "Fam. Sönnerstedt",
+            "address" => "Almgatan 6, 311 68 Slöinge"
+        ],
+        [
+            "name"    => "Dennis Johansson",
+            "email"   => "dempa74@icloud.com",
+            "address" => "Trastgatan 2a, 311 68 Slöinge"
+        ],
+        [
+            "name"    => "Anne-Sofie Yngerlöv",
+            "name2"   => "Mårten Reisne",
+            "email"   => "a_yngerlov@hotmail.com",
+            "address" => "Stationsgatan 9, 311 68 Slöinge"
+        ],
+        [
+            "name"    => "Christer Jörmalm",
+            "email"   => "cjormalm@gmail.com",
+            "address" => "Trädgårdsgatan 5, 311 68 Slöinge"
+        ],
+        [
+            "name"    => "Peter Persson",
+            "email"   => "petepers@bredband.net",
+            "address" => "Bertevägen 7, 311 67 Slöinge"
+        ],
+        [
+            "name"    => "Niclas Johansson",
+            "email"   => "krabbfiskaren@hotmail.com",
+            "address" => "Ågatan 3, 311 68 Slöinge"
+        ],
+        [
+            "name"    => "Fredrik Norén",
+            "email"   => "vallagurun@telia.com",
+            "address" => "Rosgatan 4, 311 67 Slöinge"
+        ]
+    ];
+
+
+    return Response::json($intressenter, 200, ['Access-Control-Allow-Origin' => '*']);
+});
